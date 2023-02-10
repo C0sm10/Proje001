3d24016024d86557ec326bdb8acb9ab8915b474b
C0SM10 <c0sm10ot@gmail.com>
2023-02-10 22:47:09 +0300

    hero section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,16 @@ module.exports = {
           gemunu: ['Gemunu Libre', 'sans-serif'],
           open: ['Open Sans', 'sans-serif'],
           },
+
         colors: {
           'gega-red': '#BC1A45',
           'gega-melon': '#FFD369',
           'gega-grey': '#DDDDDD',
           'gega-white': '#F7F7F7',
+      },
+      
+      spacing:{
+        '128' : '32rem',
       },
     },
   },
